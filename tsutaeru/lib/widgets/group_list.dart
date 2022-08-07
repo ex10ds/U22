95d3d4078b8c3ea55d43062b2c8a889a6d1c40de
mitsuYashi <hm20011027@gmail.com>
2022-08-08 01:36:13 +0900

    fix: refacter

--- tsutaeru/lib/widgets/group_list.dart
+++ tsutaeru/lib/widgets/group_list.dart
@@ -1,5 +1,4 @@
 // グループ一覧表示Widget
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tsutaeru/widgets/group_list_item.dart';
 
@@ -11,27 +10,18 @@ class GroupList extends StatefulWidget {
 }
 
 class _GroupListState extends State<GroupList> {
-  List<String> languages = ["Dart", "Java", "Ruby", "PHP"];
+  List<String> places = ["コンビニ", "スーパー", "サイゼリヤ"];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      // body: GridView.count(
-      //   primary: false,
-      //   padding: EdgeInsets.all(20),
-      //   crossAxisSpacing: 10,
-      //   mainAxisSpacing: 10,
-      //   crossAxisCount: 2,
-      //   children: languages.map((String title) =>
-      //     return _GroupItem(title);
-      //   ).toList(),
-      // ),
       body: GridView.count(
         crossAxisSpacing: 2,
         mainAxisSpacing: 8,
         crossAxisCount: 2,
-        children: languages
+        padding: const EdgeInsets.only(top: 40),
+        children: places
             .map((String title) => GroupListItem(
                   title: title,
                 ))
@@ -39,24 +29,4 @@ class _GroupListState extends State<GroupList> {
       ),
     ));
   }
-
-  // Widget _groupItem(String title) {
-  //   return GestureDetector(
-  //     onTap: () {
-  //       // print('Button Click');
-  //     },
-  //     child: Container(
-  //       height: 100.0,
-  //       padding: const EdgeInsets.all(8.0),
-  //       margin: const EdgeInsets.symmetric(horizontal: 8.0),
-  //       decoration: BoxDecoration(
-  //         borderRadius: BorderRadius.circular(5.0),
-  //         color: Colors.lightGreen[500],
-  //       ),
-  //       child: Center(
-  //         child: Text(title),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
