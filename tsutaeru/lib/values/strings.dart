d424e55850e1eb8e1bfe23ca46f18f634403b33d
hkiku482 <h.kikuchi482@gmail.com>
2022-08-23 22:11:26 +0900

    improved data structure of strings.dart

--- tsutaeru/lib/values/strings.dart
+++ tsutaeru/lib/values/strings.dart
@@ -1,1 +1,9 @@
-const appName = "つたえる";
+class AppString {
+  static const appName = "つたえる";
+  static const delete = "削除";
+  static const allPhrases = "すべての文章";
+  static const add = "新規登録";
+  static const phrase = "フレーズ";
+  static const addPhrase = "フレーズを追加";
+  static const inputNewPhrase = "ここにフレーズを入力してください";
+}
