12eeb8e353986b3ede6d1acdd6f67b0f1c7601e2
mitsuYashi <hm20011027@gmail.com>
2022-08-26 17:22:23 +0900

    feat: path

--- tsutaeru/lib/widgets/phrase_display.dart
+++ tsutaeru/lib/widgets/phrase_display.dart
@@ -13,28 +13,65 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class PhraseDisplay extends StatelessWidget {
-  String displayText;
+// class PhraseDisplay extends StatelessWidget {
+//   String displayText;
 
-  PhraseDisplay({super.key, required this.displayText});
+//   PhraseDisplay({super.key, required this.displayText});
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.white,
+//         body: Stack(children: [
+//           GestureDetector(
+//               onTap: () => {Navigator.of(context).pop(context)},
+//               child: Align(
+//                 alignment: Alignment.centerRight,
+//                 child: Container(
+//                   height: double.infinity,
+//                   width: 20,
+//                   color: Colors.indigo,
+//                 ),
+//               )),
+//           Center(
+//             child: Transform.rotate(
+//               angle: 90 * pi / 180,
+//               child: Container(
+//                 color: Colors.white,
+//                 child: Text(
+//                   displayText,
+//                   style: const TextStyle(
+//                     fontSize: 50,
+//                     color: Colors.black,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
+
+class PhraseDisplay extends StatefulWidget {
+  final String displayText;
+  const PhraseDisplay({Key? key, required this.displayText}) : super(key: key);
+
+  @override
+  State<PhraseDisplay> createState() => _PhraseDisplayState();
+}
+
+class _PhraseDisplayState extends State<PhraseDisplay> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Stack(children: [
-          // GestureDetector(
-          //   onTap: () => {Navigator.of(context).pop()},
-          //   child: Container(
-          //     alignment: Alignment.centerRight,
-          //     width: 20,
-          //     height: 300,
-          //     color: Colors.indigo,
-          //   ),
-          // ),
           GestureDetector(
-              onTap: () => {Navigator.of(context).pop},
+              onTap: () => {Navigator.of(context).pop(context)},
               child: Align(
                 alignment: Alignment.centerRight,
                 child: Container(
@@ -49,8 +86,8 @@ class PhraseDisplay extends StatelessWidget {
               child: Container(
                 color: Colors.white,
                 child: Text(
-                  displayText,
-                  style: TextStyle(
+                  widget.displayText,
+                  style: const TextStyle(
                     fontSize: 50,
                     color: Colors.black,
                   ),
