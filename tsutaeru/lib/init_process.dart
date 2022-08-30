35bf7ff0fee9abf567f924d4743c5021f6795098
hkiku482 <h.kikuchi482@gmail.com>
2022-08-30 11:26:12 +0900

    fix: colors

--- tsutaeru/lib/init_process.dart
+++ tsutaeru/lib/init_process.dart
@@ -1,17 +1,5 @@
-import 'package:tsutaeru/models/color.dart';
 import 'package:tsutaeru/models/init.dart';
-import 'package:tsutaeru/values/colors.dart';
 
 Future<void> initProcess() async {
   await initDatabase();
-}
-
-Future<Color> insertDefaultColor() async {
-  Color defaultColor = Color();
-  defaultColor.id = "default_color";
-  defaultColor.textColor = AppColor.defaultTextColor;
-  defaultColor.backgroundColor = AppColor.defaultBackgroungColor;
-  defaultColor.create();
-
-  return defaultColor;
 }
--- tsutaeru/lib/models/init.dart
+++ tsutaeru/lib/models/init.dart
@@ -7,6 +7,7 @@ import 'package:tsutaeru/models/phrase_group.dart';
 import 'package:tsutaeru/models/phrase.dart';
 import 'package:tsutaeru/models/settings.dart';
 import 'package:tsutaeru/values/internal.dart';
+import 'package:tsutaeru/values/colors.dart';
 
 Future<void> initDatabase() async {
   await openDatabase(join(await getDatabasesPath(), databaseName),
@@ -18,6 +19,12 @@ Future<void> initDatabase() async {
     await db.execute(Image().tableSchema.getCreateTableSql());
     await db.execute(UnsafePhraseBelonging().tableSchema.getCreateTableSql());
   }, version: databaseVersion);
+
+  Color defaultColor = Color();
+  defaultColor.id = "default_color";
+  defaultColor.textColor = AppColor.defaultTextColor;
+  defaultColor.backgroundColor = AppColor.defaultBackgroungColor;
+  defaultColor.create();
 
   Settings setting = Settings();
   await setting.check();
